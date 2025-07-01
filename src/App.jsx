4ce493b2291ab089bcf28d31d29f119ecf803be3
vader9595 <Fermiparadox1234@gmail.com>
2025-07-01 16:36:52 -0500

    Fix: remove unused Router variable to pass ESLint

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import './App.css';
 
