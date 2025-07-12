a743294933c931a8272601f0b470d3b06cf415d8
vader9595 <Fermiparadox1234@gmail.com>
2025-07-12 18:28:39 -0500

    made updates

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,9 +1,20 @@
 // components/ThankYou.jsx
 export default function ThankYou() {
+
+
+
+
   return (
+
     <div className="thank-you">
+
       <h1>Thanks for applying! 🥞</h1>
+
       <p>We’ll review your resume and get back to you soon.</p>
+      
     </div>
+
+
+
   );
 }
