7c7b402350bb9e5e23ded8e4a4a00baa66caf664
vader9595 <Fermiparadox1234@gmail.com>
2025-07-12 19:51:32 -0500

    Add ThankYou route and component

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,4 +1,7 @@
 // components/ThankYou.jsx
+
+
+
 export default function ThankYou() {
 
 
@@ -11,7 +14,7 @@ export default function ThankYou() {
       <h1>Thanks for applying! 🥞</h1>
 
       <p>We’ll review your resume and get back to you soon.</p>
-      
+
     </div>
 
 
